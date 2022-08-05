3a26bddb4ec6d9fb4c409dbb05f46f84f16741f7
Bacho <hidden@gmail.com>
2022-08-06 01:41:20 +0400

    fix svg tags

--- src/components/BottomNavigationTabBar.js
+++ src/components/BottomNavigationTabBar.js
@@ -1,6 +1,6 @@
 import React, { useState, Fragment, useEffect, useContext, useRef } from 'react';
 import styled from 'styled-components';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import HomeIcon from 'assets/media/bottomBar/Home';
 import ShopIcon from 'assets/media/bottomBar/Shop';
 import CartIcon from 'assets/media/bottomBar/Cart';
@@ -10,22 +10,22 @@ const BottomNavigationTabBar = ({showRightSide}) => {
 
     return (
         <BottomNavigationTabBarContainer>
-            <Item>
+            <NavLink to="/">
                 <HomeIcon color="#000"/>
                 <Text>Home</Text>
-            </Item>
-            <Item>
+            </NavLink>
+            <NavLink to="/shop">
                 <ShopIcon color="#000"/>
                 <Text>Shop</Text>
-            </Item>
-            <Item>
+            </NavLink>
+            <NavLink to="/cart">
                 <CartIcon color="#000"/>
                 <Text>Cart</Text>
-            </Item>
-            <Item>
+            </NavLink>
+            <NavLink  to="/profile">
                 <ProfileIcon color="#000"/>
                 <Text>Profile</Text>
-            </Item>
+            </NavLink>
         </BottomNavigationTabBarContainer>        
     )
 }
@@ -41,16 +41,17 @@ const BottomNavigationTabBarContainer = styled.div `
     justify-content: center;
     align-items: center;
     flex-direction: row;
+    & > a {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+        flex: 1;
+        color: #000;
+    }
 `
 const Text = styled.div `
     font-size: 12px;
     margin-top: 2px;
-`
-const Item = styled.div `
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction: column;
-    flex: 1
 `
 export default BottomNavigationTabBar;
